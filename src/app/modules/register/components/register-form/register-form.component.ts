8b4ba2bcc0f5d44068a70cf2464d7341c112b232
Favour Chukwuemeka <kaosemeka@yahoo.com>
2024-02-18 18:11:42 +0100

    updated register form

--- src/app/modules/register/components/register-form/register-form.component.ts
+++ src/app/modules/register/components/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonItem, IonInput, IonLabel, IonButton, IonTextarea } from '@ionic/angular/standalone';
 import { PhonenumberInputComponent } from 'src/app/shared/components';
 
@@ -15,5 +15,39 @@ import { PhonenumberInputComponent } from 'src/app/shared/components';
   ]
 })
 export class RegisterFormComponent {
-
+  @Output() onsubmit = new EventEmitter();
+  
+  registerForm: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    pensiontype: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
+    phonenumber: new FormControl('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    firstname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(1), Validators.pattern('^[a-zA-Z0-9_.+-]+$')])),
+    middlename: new FormControl('', Validators.compose([Validators.required, Validators.minLength(1), Validators.pattern('^[a-zA-Z0-9_.+-]+$')])),
+    surname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(1), Validators.pattern('^[a-zA-Z0-9_.+-]+$')]))
+  }); 
+  validation_messages = {
+    email: [
+      { type: "required", message: "Email is required." },
+      {
+        type: "minlength",
+        message: "Email must be at least 5 characters long."
+      }
+    ],
+    firstname: [
+      { type: "required", message: "Firstname is required." },
+      { type: "minlength", ssage: "Firstname must be at least 1 characters long." }
+    ],
+    surname: [
+      { type: "required", message: "Surname is required." },
+      { type: "minlength", message: "Surname must be at least 1 characters long."}
+    ],
+    middlename: [
+      { type: "required", message: "Middlename is required." },
+      { type: "minlength", message: "Middlename must be at least 1 characters long."}
+    ],
+    phonenumber: [
+      { type: "required", message: "Phone Number is required." },
+      { type: "minlength", message: "Phone Number must be at least 1 characters long."}
+    ]
+  };
 }
--- src/app/modules/register/register.page.ts
+++ src/app/modules/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IonButton, IonButtons, IonHeader, IonItem, IonLabel, IonListHeader, IonToolbar, IonContent, IonFooter } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { registerModel } from 'src/app/core';
 
 @Component({
   selector: 'app-register',
@@ -15,4 +16,13 @@ import { RegisterFormComponent } from './components/register-form/register-form.
   ]
 })
 export class RegisterPage {
+
+  register(form: registerModel) {
+
+    // this.uData.register(form).then((res: any) => {
+    //   this.globals.navigate('/login', false)
+    // }).catch((ex: any) => {
+    //   this.globals.notifyAlert("Error", ex)
+    // })
+  }
 }
